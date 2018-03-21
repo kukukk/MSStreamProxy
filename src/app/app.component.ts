686d9ea1100cd4cabba19d45995de1d8b09fe3d0
Szabolcs Balogh <bszabolcs@gmail.com>
2018-03-21 13:59:24 +0200

    Make it work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,61 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {HttpClient} from '@angular/common/http';
 
-import { HomePage } from '../pages/home/home';
+import {HomePage} from '../pages/home/home';
+
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+    rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public http: HttpClient) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+
+            this.createServer();
+        });
+    }
+
+    createServer() {
+        const that = this;
+        (<any>window).webserver.onRequest(
+            function(request) {
+                const filename = request.path.replace('/', '');
+                that.getRemoteFile(filename)
+                    .then(content => {
+                        (<any>window).webserver.sendResponse(
+                            request.requestId,
+                            content
+                        );
+                    });
+            }
+        );
+        (<any>window).webserver.start();
+    }
+
+    getRemoteFile(filename) {
+        const url = `https://kukukk.go.ro/dash/${filename}`;
+        console.log(url);
+        return new Promise(resolve => {
+            this.http
+                .get(url, {responseType: 'arraybuffer'})
+                .subscribe(
+                    data => {
+                        console.log(data);
+                        resolve(data);
+                    },
+                    error => {
+                        console.log(error);
+                        resolve('Missing...');
+                    });
+        });
+    }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    @ViewChild('video') video: any;
 
-  }
+    constructor(public navCtrl: NavController, public http: HttpClient) {
 
+    }
+
+    playVideo() {
+        const video = this.video.nativeElement;
+        video.src = 'https://kukukk.go.ro/dash/base.m3u8';
+        video.play();
+    }
 }
